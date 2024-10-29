88e6540e6f41525dfb45e4ec363a2a1b32ff0e12
ALEX <a.yuhim@mail.ru>
2024-10-29 21:53:59 +0300

    feat: создает вспомогательный файл utils

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './scss/style.scss';
+import { dataTreks } from './utils';
+
+const root = document.getElementById('app');
+const audioplayer = document.createElement(div);
+audioplayer.classList.add('wr');
+
+root.append(audioplayer);
---
+++ src/utils.js
@@ -0,0 +1,17 @@
+export const dataTreks = [
+  {
+    icons: './icons/sun.svg',
+    background: './img/summer-bg.jpg',
+    trek: './sounds/summer.mp3',
+  },
+  {
+    icons: './icons/cloud-rain.svg',
+    background: './img/rainy-bg.jpg',
+    trek: './sounds/rain.mp3',
+  },
+  {
+    icons: './icons/cloud-rain.svg',
+    background: './img/winter-bg.jpg',
+    trek: './sounds/winter.mp3',
+  },
+];

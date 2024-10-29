446bed2a37321123ac5396864c68821d757703c3
ALEX <a.yuhim@mail.ru>
2024-10-29 21:08:59 +0300

    fix:добавляет правило esLinta

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,10 +15,12 @@ module.exports = {
   extends: ['eslint:recommended', 'google'],
   rules: {
     'jsdoc/newline-after-description': 'off',
+    'object-curly-spacing': 'off',
     'linebreak-style': 'off',
     'eol-last': 0,
     'require-jsdoc': 'off',
     // eslint-disable-next-line quote-props
     semi: 'off',
+    'space-before-function-paren': 'off',
   },
 };

670650886d6e5826196704c0c42cb8b911f14267
ALEX <a.yuhim@mail.ru>
2024-11-16 17:01:48 +0300

    fix:Изменяет согласно коментариям PR

--- src/API.ts
+++ src/API.ts
@@ -1,38 +1,40 @@
-export const dataTreks = [
+import { IWeatherItem } from './types/WeatherItems';
+
+export const dataTreks: IWeatherItem[] = [
   {
     id: '1',
-    icons: './assets/icons/sun.svg',
+    icon: './assets/icons/sun.svg',
     background: './assets/img/summer-bg.jpg',
-    trek: './assets/sounds/summer.mp3',
+    track: './assets/sounds/summer.mp3',
   },
   {
     id: '2',
-    icons: './assets/icons/cloud-rain.svg',
+    icon: './assets/icons/cloud-rain.svg',
     background: './assets/img/rainy-bg.jpg',
-    trek: './assets/sounds/rain.mp3',
+    track: './assets/sounds/rain.mp3',
   },
   {
     id: '3',
-    icons: './assets/icons/cloud-snow.svg',
+    icon: './assets/icons/cloud-snow.svg',
     background: './assets/img/winter-bg.jpg',
-    trek: './assets/sounds/winter.mp3',
+    track: './assets/sounds/winter.mp3',
   },
   {
     id: '4',
-    icons: './assets/icons/sun.svg',
+    icon: './assets/icons/sun.svg',
     background: './assets/img/summer-bg.jpg',
-    trek: './assets/sounds/summer.mp3',
+    track: './assets/sounds/summer.mp3',
   },
   {
     id: '5',
-    icons: './assets/icons/cloud-rain.svg',
+    icon: './assets/icons/cloud-rain.svg',
     background: './assets/img/rainy-bg.jpg',
-    trek: './assets/sounds/rain.mp3',
+    track: './assets/sounds/rain.mp3',
   },
   {
     id: '6',
-    icons: './assets/icons/cloud-snow.svg',
+    icon: './assets/icons/cloud-snow.svg',
     background: './assets/img/winter-bg.jpg',
-    trek: './assets/sounds/winter.mp3',
+    track: './assets/sounds/winter.mp3',
   },
 ];
